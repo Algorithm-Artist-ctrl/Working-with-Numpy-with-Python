76b56c8cd2d89d62cd67cecf4145999a25fb579c
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-10 22:27:32 +0530

    Creates a table (DataFrame)

---
+++ pandas4.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Create a simple DataFrame
+data = {
+    'Name': ['Aman', 'Riya', 'Sohan'],
+    'Age': [20, 21, 19],
+    'Marks': [85, 90, 78]
+}
+
+df = pd.DataFrame(data)
+print("DataFrame:")
+print(df)
+'''
+print("\nFirst rows:")
+print(df.head())
+print("\nNames:")
+print(df['Name'])
+print("\nStudents with marks > 80:")
+print(df[df['Marks'] > 80])
+df['Result'] = ['Pass', 'Pass', 'Pass']
+
+print("\nAfter adding new column:")
+print(df)'''
