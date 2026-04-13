22e08b778b450d4cdf0d490872607bcf6bcf3dac
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-13 21:23:21 +0530

    To print rows

--- pandas5.py
+++ pandas5.py
@@ -9,11 +9,3 @@ data = {
 df = pd.DataFrame(data)
 print("\nFirst rows:")
 print(df.head())
-'''print("\nNames:")
-print(df['Name'])
-print("\nStudents with marks > 80:")
-print(df[df['Marks'] > 80])
-df['Result'] = ['Pass', 'Pass', 'Pass']
-
-print("\nAfter adding new column:")
-print(df)'''
