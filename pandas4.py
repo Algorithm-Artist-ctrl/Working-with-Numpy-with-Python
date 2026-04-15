75204e8f2bb467a539551c7e03f0d158e8df433f
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-15 20:20:18 +0530

    Print data

--- pandas4.py
+++ pandas4.py
@@ -10,14 +10,3 @@ data = {
 df = pd.DataFrame(data)
 print("DataFrame:")
 print(df)
-'''
-print("\nFirst rows:")
-print(df.head())
-print("\nNames:")
-print(df['Name'])
-print("\nStudents with marks > 80:")
-print(df[df['Marks'] > 80])
-df['Result'] = ['Pass', 'Pass', 'Pass']
-
-print("\nAfter adding new column:")
-print(df)'''
