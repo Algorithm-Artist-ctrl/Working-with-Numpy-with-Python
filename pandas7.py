a2f5044cce9a0aa71555a8efda9b1181f6d1fe9e
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-17 23:07:00 +0530

    Print student with makrks

---
+++ pandas7.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = {
+    'Name': ['Aman', 'Riya', 'Sohan'],
+    'Age': [20, 21, 19],
+    'Marks': [85, 90, 78]
+}
+df = pd.DataFrame(data)
+print("\nNames:")
+print(df['Name'])
+
+print("\nStudents with marks > 80:")
+''''
+print(df[df['Marks'] > 80])
+df['Result'] = ['Pass', 'Pass', 'Pass']
+
+print("\nAfter adding new column:")
+print(df)'''
