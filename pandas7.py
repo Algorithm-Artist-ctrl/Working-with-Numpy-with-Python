d34a9329adf6f81d409007b00e4b23709987e7f5
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-20 22:57:44 +0530

    Printing students wih marks

--- pandas7.py
+++ pandas7.py
@@ -8,11 +8,4 @@ data = {
 df = pd.DataFrame(data)
 print("\nNames:")
 print(df['Name'])
-
 print("\nStudents with marks > 80:")
-''''
-print(df[df['Marks'] > 80])
-df['Result'] = ['Pass', 'Pass', 'Pass']
-
-print("\nAfter adding new column:")
-print(df)'''
