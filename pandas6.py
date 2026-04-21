fe4d1577e0303f27b98a5ec21ab555e930016baf
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-21 21:18:27 +0530

    Printing names of students

--- pandas6.py
+++ pandas6.py
@@ -8,10 +8,3 @@ data = {
 df = pd.DataFrame(data)
 print("\nNames:")
 print(df['Name'])
-'''
-print("\nStudents with marks > 80:")
-print(df[df['Marks'] > 80])
-df['Result'] = ['Pass', 'Pass', 'Pass']
-
-print("\nAfter adding new column:")
-print(df)'''
