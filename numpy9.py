f74610c70b678a717e47e65d4af2b4e445c82ecb
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-04-25 22:19:35 +0530

    Max and minimum value in array

--- numpy9.py
+++ numpy9.py
@@ -1,5 +1,5 @@
 import numpy as np
-arr = np.array([10, 25, 5, 40, 15])
+arr = np.array([10, 25, 5, 40,89, 15])
 print("Array:", arr)
 print("Maximum value:", np.max(arr))
 print("Minimum value:", np.min(arr))
