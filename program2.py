b5a61727012ed2210f54af51ccc58b5f6cc75059
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-09 22:47:17 +0530

    Student Marks Analysis

---
+++ program2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+marks = np.array([78, 85, 90, 66, 88])
+df = pd.DataFrame({
+    "Marks": marks
+})
+print("Average:", np.mean(df["Marks"]))
+print("Highest:", np.max(df["Marks"]))
+print("Lowest:", np.min(df["Marks"]))
