64730ef0465db5d8fa1d5cb8eb5bf079979eebac
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-11 22:14:32 +0530

    Add NumPy Calculations to DataFrame

---
+++ program4.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+data = {
+    "Math": [80, 70, 90],
+    "Science": [75, 85, 95]
+}
+df = pd.DataFrame(data)
+df["Total"] = np.sum(df[["Math", "Science"]], axis=1)
+df["Percentage"] = df["Total"] / 2
+
+print(df)
