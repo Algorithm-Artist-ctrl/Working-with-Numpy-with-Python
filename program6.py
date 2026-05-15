719e45efbc649cdf16721d748f38938201cc8a2f
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-15 22:47:51 +0530

    Employee Salary Statistics

---
+++ program6.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+salary = np.array([25000, 30000, 28000, 35000, 40000])
+
+df = pd.DataFrame({
+    "Salary": salary
+})
+print("Mean Salary:", np.mean(df["Salary"]))
+print("Standard Deviation:", np.std(df["Salary"]))
