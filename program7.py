a08588b07a4919034ce6bfbb682a55c413e18d5e
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-17 22:52:31 +0530

    Handle Missing Data

---
+++ program7.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+data = {
+    "Marks": [90, np.nan, 85, np.nan, 95]
+}
+df = pd.DataFrame(data)
+average = np.nanmean(df["Marks"])
+df["Marks"].fillna(average, inplace=True)
+print(df)
