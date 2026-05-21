9b95a5dcf5a9516bd8a45e90a8f2e3ee6d101c11
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-21 23:22:01 +0530

    Data Analysis on Temperature Records

---
+++ program10.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+temperature = np.array([30, 32, 31, 29, 35, 36, 33])
+df = pd.DataFrame({
+    "Temperature": temperature
+})
+print(df)
+print("\nAverage Temperature:", np.mean(df["Temperature"]))
+print("Maximum Temperature:", np.max(df["Temperature"]))
+print("Minimum Temperature:", np.min(df["Temperature"]))
