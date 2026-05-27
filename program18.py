97462aa210ec69e8362bd2e297930483bc6f2027
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-05-27 16:15:18 +0530

    temperature conveter

---
+++ program18.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+celsius = np.array([0, 20, 30, 40])
+
+fahrenheit = (celsius * 9/5) + 32
+
+df = pd.DataFrame({
+    "Celsius": celsius,
+    "Fahrenheit": fahrenheit
+})
+
+print(df)
