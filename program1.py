47b042715cbe657e431cb341ef06b023c9455398
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-06-06 20:02:06 +0530

    Arrange Data

--- program1.py
+++ program1.py
@@ -1,5 +1,5 @@
 import numpy as np
 import pandas as pd
 arr = np.arange(1, 13).reshape(3, 4)
-df = pd.DataFrame(arr, columns=["A", "B", "C", "D"])
+df = pd.DataFrame(arr, columns=["A", "B", "C", "D","E"])
 print(df)
