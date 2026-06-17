41a79b4160c383578f1aa0e4e5b52e3754c337ed
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-06-17 19:16:03 +0530

    Temperature

--- program11.py
+++ program11.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-temperature = np.array([30, 32, 31, 29, 35, 36, 33])
+temperature = np.array([30, 32, 31, 78,87,29, 35, 36, 33])
 df = pd.DataFrame({
     "Temperature": temperature
 })
